39e29efcd98734a7daec265df8330efe6bfa9bb8
Tendai Karuwo <tendaikaruwo96@gmail.com>
2023-10-12 12:37:44 +0100

    created react app

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Header from './components/Header/Header'
+import Calculator from './components/Calculator/Calculator';
+
+function App() {
+  return (
+    <div className='main-background'>
+      <Header />
+      <Calculator />
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/components/Calculator/Calculator.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Calculator.css'
+
+export default function Calculator() {
+  return (
+      <div id='calc'>
+          
+    </div>
+  )
+}
